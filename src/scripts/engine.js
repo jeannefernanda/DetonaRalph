e30514c336dcd3d4eb29822871171fb3ed1587db
Jeanne <jeannefernanda.m@gmail.com>
2024-01-30 17:13:38 -0400

    Implementando actions

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -6,12 +6,28 @@ const state = {
     score: document.querySelector("#score"),
   },
   values: {
-    timerId: null,
     gameVelocity: 1000,
     hitPosition: 0,
     result: 0,
+    currentTime: 60,
+  },
+
+  actions: {
+    timerId: setInterval(randomSquare, 1000),
+    countDownTimerId: setInterval(countDown, 1000),
   },
 };
+
+function countDown() {
+    state.values.currentTime--;
+    state.view.timeLeft.textContent = state.values.currentTime;
+
+    if(state.values.currentTime <= 0) {
+        clearInterval(state.actions.countDownTimerId);
+        clearInterval(state.actions.timerId);
+        alert("Game Over! O seu resultado foi: " + state.values.result);
+    }
+}
 
 function randomSquare() {
   state.view.squares.forEach((square) => {
@@ -25,9 +41,9 @@ function randomSquare() {
   state.values.hitPosition = randomSquare.id;
 }
 
-function moveEnemy() {
-  state.values.timerId = setInterval(randomSquare, state.values.gameVelocity);
-}
+// function moveEnemy() {
+//   state.values.timerId = setInterval(randomSquare, state.values.gameVelocity);
+// }
 
 function addListenerHitBox() {
   state.view.squares.forEach((square) => {
@@ -42,7 +58,7 @@ function addListenerHitBox() {
 }
 
 function initialize() {
-  moveEnemy();
+//   moveEnemy();
   addListenerHitBox();
 }
 
